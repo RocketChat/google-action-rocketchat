0002306f8a267c86cc013f06573193805890f7b1
Adarsh Naidu <naidu.adarsh2511@gmail.com>
2020-07-18 18:05:57 +0530

    adding environment variables and local development script

--- functions/config.js
+++ functions/config.js
@@ -1,14 +1,13 @@
 // Environment Variables
-const functions = require('firebase-functions');
-
-
-if(process.env.DEVELOPMENT) {
+if (process.env.DEVELOPMENT) {
+    require('dotenv').config();
     module.exports = {
-        SERVER_URL: 'https://bots.rocket.chat',
-        CLIENT_ID: 'RHzbmCGN9zZD793oJ',
-        OAUTH_SERVICE_NAME: 'googleactiontest'
+        SERVER_URL: process.env.SERVER_URL,
+        CLIENT_ID: process.env.CLIENT_ID,
+        OAUTH_SERVICE_NAME: process.env.OAUTH_SERVICE_NAME
     }
 } else {
+    const functions = require('firebase-functions');
     module.exports = {
         SERVER_URL : functions.config().envariables.server_url,
         CLIENT_ID : functions.config().envariables.clientid,
