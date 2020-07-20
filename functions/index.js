ac9c45a4bec2e375a05eb350191ce9955647e909
Adarsh Naidu <naidu.adarsh2511@gmail.com>
2020-07-20 14:04:29 +0530

    set announcement intent handlers

--- functions/index.js
+++ functions/index.js
@@ -923,44 +923,35 @@ app.intent('Channel Description Intent', async (conv, params) => {
 
 });
 
-app.intent('Channel Announcement Intent', async (conv, params) => {
+app.intent('Set Announcement Intent Slot Collection', async (conv, params) => {
+  const accessToken = conv.user.access.token;
+  const headers = await helperFunctions.login(accessToken);
+  let channelname = params.channelname;
+  const announcement = params.announcement;
 
   var locale = conv.user.locale;
-
-  if (locale === 'hi-IN') {
-
-    var accessToken = conv.user.access.token;
-
-    var channelNameRaw = params.channelname;
-    var channelNameData = await helperFunctions.hinditranslate(channelNameRaw);
-    var channelNameLwr = channelNameData.toLowerCase();
-    var channelName = helperFunctions.replaceWhitespacesFunc(channelNameLwr);
-
-    var announcement = params.announcement;
-
-    const headers = await helperFunctions.login(accessToken);
-    const roomid = await helperFunctions.getRoomId(channelName, headers);
-    const speechText = await helperFunctions.channelAnnouncement(channelName, roomid, announcement, headers);
-
-    conv.ask(speechText);
-
-  } else {
-
-    var accessToken = conv.user.access.token;
-
-    var channelNameRaw = params.channelname;
-    var channelNameData = channelNameRaw.toLowerCase();
-    var channelName = helperFunctions.replaceWhitespacesFunc(channelNameData);
-
-    var announcement = params.announcement;
-
-    const headers = await helperFunctions.login(accessToken);
-    const roomid = await helperFunctions.getRoomId(channelName, headers);
-    const speechText = await helperFunctions.channelAnnouncement(channelName, roomid, announcement, headers);
-
-    conv.ask(speechText);
-
+  if(locale === 'hi-IN') {
+    channelname = await helperFunctions.hinditranslate(channelname);
   }
+
+  const channelDetails = await helperFunctions.resolveChannelname(channelname, headers);
+
+  if(channelDetails) {
+    conv.ask(i18n.__('CHANNEL_ANNOUNCEMENT.CONFIRM_INTENT', announcement, channelDetails.name))
+    conv.data.channelDetails = channelDetails
+    conv.contexts.set('set_announcement', 1, {channelname, announcement})
+  } else {
+    conv.ask(i18n.__('CHANNEL_ANNOUNCEMENT.NO_ROOM', channelname))
+  }
+})
+
+app.intent('Set Announcement Intent Confirmed', async (conv, params) => {
+
+  var accessToken = conv.user.access.token;
+  const headers = await helperFunctions.login(accessToken);
+
+  const speechText = await helperFunctions.setAnnouncement(conv.data.channelDetails, params.announcement, headers);
+  conv.ask(speechText);
 
 });
 
